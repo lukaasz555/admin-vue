5dce2dcd281be7e06e2f66060deec838d8e356df
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-10-19 09:10:08 +0200

    feat: add base for button/input components

---
+++ src/global/components/button.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    :class="[
+      'd-flex',
+      align === 'left' ? 'justify-start' : '',
+      align === 'center' ? 'justify-center' : '',
+      align === 'right' ? 'justify-end' : '',
+    ]"
+  >
+    <v-btn
+      :block="isFullWidth"
+      :disabled="isDisabled"
+      :height="height"
+      :loading="isLoading"
+      :ripple="false"
+      :variant="variant"
+      @click="emit('click')"
+    >
+      {{ label }}
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const props = defineProps({
+  label: { type: String, default: 'Save' },
+  align: {
+    type: String as PropType<'left' | 'center' | 'right'>,
+    default: 'center',
+  },
+  variant: {
+    type: String as PropType<'outlined' | 'flat' | 'text' | 'plain'>,
+    default: 'flat',
+  },
+  height: { type: Number, default: undefined },
+  isFullWidth: { type: Boolean, default: false },
+  isDisabled: { type: Boolean, default: false },
+  isLoading: { type: Boolean, default: false },
+});
+</script>
---
+++ src/global/components/input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input--container">
+    <v-text-field
+      v-model="inputValue"
+      :label="label"
+      variant="outlined"
+    ></v-text-field>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'update:modelValue', val: string): void;
+}>();
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  label: { type: String, required: true },
+  isDisabled: { type: Boolean, default: false },
+});
+
+const inputValue = computed({
+  get: () => props.modelValue,
+  set: (val: string) => emit('update:modelValue', val),
+});
+</script>
