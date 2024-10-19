5737b9b13e2db6eeccd30b70eabe7d51d4ef0d9c
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-10-19 14:38:27 +0200

    feat: add forgot password, cleanup

--- src/modules/auth/views/auth-view.vue
+++ src/modules/auth/views/auth-view.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>AuthView</div>
+  <div class="auth--wrapper">
+    <div class="auth--wrapper__form--container">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.auth--wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  &__form--container {
+    min-width: 250px;
+    max-width: 400px;
+    width: 100%;
+    padding: 1.5rem 1.5rem 0.75rem;
+    border: 1px solid $border-color;
+    border-radius: $border-radius-xl;
+  }
+}
+</style>
