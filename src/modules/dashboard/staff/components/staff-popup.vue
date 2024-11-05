db8b069f00498db11dc6e283eff547d8f9f2d5c0
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-11-05 20:20:12 +0100

    fix: after merge

--- src/modules/dashboard/staff/components/staff-popup.vue
+++ src/modules/dashboard/staff/components/staff-popup.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
     <div>staff data/form goes here...</div>
+    <StaffForm />
   </div>
 </template>
+
+<script setup lang="ts">
+import StaffForm from './staff-form.vue';
+</script>
