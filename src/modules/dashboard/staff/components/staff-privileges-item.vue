f99b2584370a1b175199d5287fcef228c525f29e
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-11-16 11:39:46 +0100

    feat: display privileges wip

---
+++ src/modules/dashboard/staff/components/staff-privileges-item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="item">
+    Privilege: {{ privilege }}, Value: {{ currentValue }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { DashboardModulesEnum } from '../../enums/dashboard-modules.enum';
+import { Privileges as PrivilegesEnum } from '@/global/enums/privileges.enum';
+
+type PrivilegesItemEmit = {
+  (
+    e: 'update',
+    privilege: DashboardModulesEnum,
+    value: PrivilegesEnum,
+  ): void;
+};
+
+const emit = defineEmits<PrivilegesItemEmit>();
+
+const props = defineProps({
+  privilege: {
+    type: String as PropType<DashboardModulesEnum>,
+    required: true,
+  },
+  currentValue: {
+    type: String as PropType<PrivilegesEnum>,
+    required: true,
+  },
+});
+
+function updatePrivilege(): void {
+  emit('update', props.privilege, PrivilegesEnum.READONLY);
+}
+</script>
+
+<style scoped lang="scss">
+.item {
+  // ...
+}
+</style>
--- src/modules/dashboard/staff/components/staff-privileges.vue
+++ src/modules/dashboard/staff/components/staff-privileges.vue
@@ -1,3 +1,65 @@
 <template>
-  <div>staff privileges...</div>
+  <div class="privileges">
+    <StaffPrivilegesItem
+      v-for="(privilegeItem, index) in privilegesItems"
+      :key="index"
+      class="item"
+      :current-value="privilegeItem.value"
+      :privilege="privilegeItem.privilege"
+      @update="updatePrivilege"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { DashboardModulesEnum } from '../../enums/dashboard-modules.enum';
+import { Privileges } from '../models/privileges';
+import { useStaffStore } from '../staff.store';
+import { Privileges as PrivilegesEnum } from '@/global/enums/privileges.enum';
+import StaffPrivilegesItem from './staff-privileges-item.vue';
+
+const props = defineProps({
+  staffId: {
+    type: Number,
+    required: true,
+  },
+});
+
+const staffStore = useStaffStore();
+const privileges = ref(new Privileges());
+
+function updatePrivilege(
+  privilege: DashboardModulesEnum,
+  value: PrivilegesEnum,
+): void {
+  privileges.value.setPrivilege(privilege, value);
+}
+
+onMounted(() => {
+  const member = staffStore.getMember(props.staffId);
+  if (member) {
+    privileges.value = member.privileges;
+  }
+});
+
+const privilegesItems = computed(() => {
+  return Object.entries(privileges.value).map(
+    ([key, value]) =>
+      ({
+        privilege: key,
+        value,
+      }) as {
+        privilege: DashboardModulesEnum;
+        value: PrivilegesEnum;
+      },
+  );
+});
+</script>
+
+<style scoped lang="scss">
+.privileges {
+  .item {
+    // ...
+  }
+}
+</style>
