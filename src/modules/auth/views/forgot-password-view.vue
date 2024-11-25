e1cbf65e12969d5d1de32f9e99f5aab44921a388
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-11-25 19:37:33 +0100

    feat: add success message component

--- src/modules/auth/views/forgot-password-view.vue
+++ src/modules/auth/views/forgot-password-view.vue
@@ -1,23 +1,34 @@
 <template>
+  <EmailPasswordMessage
+    v-if="passwordEmail.isSent"
+    :email="passwordEmail.email"
+  />
   <ForgotPasswordForm
+    v-else
     :is-loading="isPending"
     @confirm="handleForgotPassword"
   />
 </template>
 
 <script setup lang="ts">
-import { useMutation } from '@tanstack/vue-query';
-import ForgotPasswordForm from '../components/forgot-password-form.vue';
-import { authService } from '../service/auth.service';
 import { ForgotPasswordData } from '../viewModels/forgot-password-data';
 import { useGlobalStore } from '@/global/store/global.store';
-import i18n from '@/plugins/i18n';
+import { useMutation } from '@tanstack/vue-query';
+import { authService } from '../service/auth.service';
 import { MessageType } from '@/global/enums/message-type.enum';
 import { Message } from '@/global/models/message';
+import EmailPasswordMessage from '../components/email-password-message.vue';
+import ForgotPasswordForm from '../components/forgot-password-form.vue';
+import i18n from '@/plugins/i18n';
 
 const globalStore = useGlobalStore();
+const passwordEmail = ref({
+  isSent: false,
+  email: '',
+});
 
 function handleForgotPassword(data: ForgotPasswordData): void {
+  passwordEmail.value.email = data.email;
   mutate(data);
 }
 
@@ -25,11 +36,7 @@ const { isPending, mutate } = useMutation({
   mutationFn: (data: ForgotPasswordData) =>
     authService.resetPassword(data),
   onSuccess: (data) => {
-    const message = Message.getMessage(
-      MessageType.SUCCESS,
-      i18n.global.t('Password reset email sent'),
-    );
-    globalStore.addMessage(message);
+    passwordEmail.value.isSent = true;
     return data;
   },
   onError: () => {
