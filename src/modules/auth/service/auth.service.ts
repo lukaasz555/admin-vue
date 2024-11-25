3a9b924d825d75d72877ce07f23ce401502a41fc
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-11-25 19:12:37 +0100

    feat: add base ui for password recovery & add method in authService

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -2,6 +2,7 @@ import { LoginData } from '../viewModels/login-data';
 import { IHttpClient } from '@/global/interfaces/http-client';
 import { IHttpService } from '@/global/interfaces/http-service';
 import { AxiosClient } from '@/global/utils/axios-client';
+import { ForgotPasswordData } from '../viewModels/forgot-password-data';
 
 class AuthService implements IHttpService {
   httpClient: IHttpClient;
@@ -14,6 +15,13 @@ class AuthService implements IHttpService {
     return this.httpClient.postData<LoginData, string>(
       'auth/signin',
       loginData,
+    );
+  }
+
+  async resetPassword(data: ForgotPasswordData): Promise<void> {
+    return this.httpClient.postData<ForgotPasswordData, void>(
+      'auth/forgot-password',
+      data,
     );
   }
 }
--- src/modules/auth/views/forgot-password-view.vue
+++ src/modules/auth/views/forgot-password-view.vue
@@ -1,9 +1,43 @@
 <template>
-  <ForgotPasswordForm @confirm="handleForgotPassword" />
+  <ForgotPasswordForm
+    :is-loading="isPending"
+    @confirm="handleForgotPassword"
+  />
 </template>
 
 <script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query';
 import ForgotPasswordForm from '../components/forgot-password-form.vue';
+import { authService } from '../service/auth.service';
+import { ForgotPasswordData } from '../viewModels/forgot-password-data';
+import { useGlobalStore } from '@/global/store/global.store';
+import i18n from '@/plugins/i18n';
+import { MessageType } from '@/global/enums/message-type.enum';
+import { Message } from '@/global/models/message';
 
-function handleForgotPassword(): void {}
+const globalStore = useGlobalStore();
+
+function handleForgotPassword(data: ForgotPasswordData): void {
+  mutate(data);
+}
+
+const { isPending, mutate } = useMutation({
+  mutationFn: (data: ForgotPasswordData) =>
+    authService.resetPassword(data),
+  onSuccess: (data) => {
+    const message = Message.getMessage(
+      MessageType.SUCCESS,
+      i18n.global.t('Password reset email sent'),
+    );
+    globalStore.addMessage(message);
+    return data;
+  },
+  onError: () => {
+    const message = Message.getMessage(
+      MessageType.ERROR,
+      i18n.global.t('Something went wrong'),
+    );
+    globalStore.addMessage(message);
+  },
+});
 </script>
