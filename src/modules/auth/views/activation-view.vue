cacbd6957235b99e9267942b4a79d3bb1721ead0
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-12-07 19:13:20 +0100

    feat: add activation view, UI adjustments in staff-popup, add translations

--- src/modules/auth/views/activation-view.vue
+++ src/modules/auth/views/activation-view.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="auth-view-container">
-    <AuthLoader :is-visible="true" />
+    <AuthLoader
+      :is-visible="sendActivationTokenMutation.isPending.value"
+    />
+    <div
+      v-if="sendActivationTokenMutation.isError.value"
+      class="auth-view-container--error"
+    >
+      <v-icon>mdi-alert-circle</v-icon>
+      {{ errorMessage }}
+    </div>
+    <div
+      v-if="sendActivationTokenMutation.isSuccess.value"
+      class="auth-view-container--success"
+    >
+      <header class="d-flex justify-center align-center">
+        <v-icon :size="28">mdi-check-circle</v-icon>
+        <h3>{{ $t('Your account is active') }}</h3>
+      </header>
+
+      <p class="text text-center mt-5 mb-3">
+        {{
+          $t(
+            'You will be redirected to new password form in {secondsLeftToRedirect} seconds',
+            { secondsLeftToRedirect: secondsLeftToRedirect },
+          )
+        }}
+      </p>
+      <p class="text text-center">
+        <router-link class="text--link" :to="AuthRoutesNames.LOGIN">{{
+          $t('Click here')
+        }}</router-link>
+        {{ $t('to see form now') }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -9,20 +42,49 @@ import { AuthRoutesNames } from '../enums/auth-routes-names.enum';
 import { getQueryValues } from '@/global/helpers/get-query-values';
 import { useMutation } from '@tanstack/vue-query';
 import { authService } from '../service/auth.service';
+import { AxiosError } from 'axios';
 import AuthLoader from '../components/auth-loader.vue';
 import router from '@/plugins/router';
+import i18n from '@/plugins/i18n';
+
+const errorMessage = ref('');
+const secondsLeftToRedirect = ref(6);
+let secondsToRedirectInterval: ReturnType<typeof setInterval>;
 
 const sendActivationTokenMutation = useMutation({
   mutationFn: (token: string) => authService.activateAccount(token),
   onSuccess: () => {
-    router.push({ name: AuthRoutesNames.CHANGE_PASSWORD });
+    startCounterAndRedirect();
+  },
+  onError: (err) => {
+    if (err instanceof AxiosError) {
+      errorMessage.value =
+        err.response?.data?.message ??
+        i18n.global.t('Something went wrong');
+    } else {
+      errorMessage.value = i18n.global.t('Something went wrong');
+    }
   },
 });
+
+function startCounterAndRedirect() {
+  secondsToRedirectInterval = setInterval(() => {
+    secondsLeftToRedirect.value -= 1;
+    if (secondsLeftToRedirect.value === 0) {
+      clearInterval(secondsToRedirectInterval);
+      router.push({ name: AuthRoutesNames.CHANGE_PASSWORD });
+    }
+  }, 1000);
+}
 
 onMounted(() => {
   const { token } = getQueryValues(['token']);
   if (token) {
     sendActivationTokenMutation.mutate(token);
   }
+});
+
+onUnmounted(() => {
+  clearInterval(secondsToRedirectInterval);
 });
 </script>
